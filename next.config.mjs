f8f560c04e0edc17c7b34dc90a10349942bbf11b
UgurCanUysal18 <67745196+UgurCanUysal18@users.noreply.github.com>
2025-03-28 22:32:18 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',  // This forces Next.js to generate static files
+  distDir: 'out',    // Specifies the output directory
+  images: {
+    unoptimized: true, // Required for static export
+  },
+};
 
 export default nextConfig;
